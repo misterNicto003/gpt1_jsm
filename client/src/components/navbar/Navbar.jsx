f8e1e83cca0ed444b470fda7136b79dce61a19ff
kanuni <mister.nicto006@gmail.com>
2024-02-22 18:22:12 +0300

    Navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,26 +1,48 @@
-import React from "react";
-import { RiMenu3Line, RiCloselin } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt1__navbar">
       <div className="gpt1__navbar-links">
         <div className="gpt1__navbar-links_logo">
-          <img src={logo} alt="logo" />
+          <img src={logo} />
         </div>
         <div className="gpt1__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT?</a></p>
+          <p><a href="#wgpt1">What is GPT1?</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
         </div>
       </div>
       <div className="gpt1__navbar-sign">
-        <p>Sign in </p>
+        <p>Sign in</p>
         <button type="button">Sign up</button>
+      </div>
+      <div className="gpt1__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="gpt1__navbar-menu_container scale-up-center">
+          <div className="gpt1__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt1">What is GPT1?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="gpt1__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
       </div>
     </div>
   );
